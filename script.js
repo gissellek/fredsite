a6e96b3e77753240cfffa43afe057c9acadc26f8
Gisselle Kinghorn <34711772+gissellek@users.noreply.github.com>
2020-06-26 16:41:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+lightbox.option({
+      'resizeDuration': 350,
+      'showImageNumberLabel': false
+    })
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
